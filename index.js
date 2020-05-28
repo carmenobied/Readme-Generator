814fe5072e7edcbb528261e56bfeac9f3ade07b0
Carmen Macleroy Obied, PhD <carmen.obied@gmail.com>
2020-05-27 23:31:04 -0700

    updated index.js file and added fs.writeFile code snippet for the function writeToFile

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const inquirer = require("inquirer");
 const fs = require('fs');
 const axios = require("axios");
 
-// import generateMarkdown
+// import generateMarkdown file
 const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // questions array
@@ -26,19 +26,29 @@ const questions = [
 // * Contributing
 // * Tests
 
-// fs module function to create a README
+// writeToFile function with file-extend (fs) module function to create a README
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err) {
+        if (err) {
+          console.log(err)
+        }else {
+          console.log("Success!")
+        }
+      });
 }
 
 // init function to display questions
 function init() {
-
+}
 // create badge via http://shields.io/
+generateMarkdown();
 
-// retrieving user's Github information via the Github API
+// retrieve user's Github information via the Github API
 
 // call writeToFile() function
-}
+writeToFile();
 
 // call init() function
 init();
+
+// command git init to initialise local Git repo
