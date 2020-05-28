5af30e9805ee369a62a6c1efad5af771a00e25b3
Carmen Macleroy Obied, PhD <carmen.obied@gmail.com>
2020-05-27 23:58:33 -0700

    updated index.js and added questions to questions array and updated the functions

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
 // require constants 
-const inquirer = require("inquirer");
 const fs = require('fs');
+const inquirer = require("inquirer");
 const axios = require("axios");
-
 // import generateMarkdown file
 const generateMarkdown = require("./utils/generateMarkdown.js");
+const writeFileAsync = util.promisify(fs.writeFile);
+let userName;
 
 // questions array
 const questions = [
     { 
         type: "input",
         message: "What is your GitHub username?",
-        name: "username"
+        name: "Username"
       },
+      { 
+        type: "input",
+        message: "What is your Project Title?",
+        name: "Title"
+      },
+    {
+        type: "input",
+        message: "Please include a short project description.",
+        name: "Description"
+    },
+    {
+        type: "input",
+        message: "What commands are required to install the project and its dependencies?",
+        name: "Installation"
+    },
+    {
+        type: "input",
+        message: "Please provide repo usage instructions.",        
+        name: "Usage",
+    },
+    {
+        type: "input",
+        message: "Please enter the License name if aplicable, otherwise enter N/A.",
+        name: "License"
+    },
+    {
+        type: "input",
+        message: "Please include Github usernames of project contributors, if applicable",
+        name: "Contributing"
+    },
+    {
+        type: "input",
+        message: "What commands are required to run tests?",
+        name: "Tests"
+    },
 ];
 
 // Questions:
@@ -39,8 +75,10 @@ function writeToFile(fileName, data) {
 
 // init function to display questions
 function init() {
+    return inquirer.prompt([])
 }
 // create badge via http://shields.io/
+const badge = `![GitHub repo size](https://img.shields.io/github/repo-size/${Username}/${Title}?logo=github)`
 
 // call generateMarkdown
 generateMarkdown();
