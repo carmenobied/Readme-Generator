245df8b9da7d35d248633367cbd00c8fa377881c
Carmen Macleroy Obied, PhD <carmen.obied@gmail.com>
2020-06-04 12:40:37 -0700

    refactored questions function.

--- README.md
+++ README.md
@@ -5,9 +5,7 @@
 ![Project license badge](https://img.shields.io/badge/license-MIT-brightgreen)      
 
   # Description
-  The goal was to create a command-line application that dynamically generates a README.md from a user's input. 
-  The application is invoked with the node index.js command. 
-  The user is prompted for their GitHub username and other information pertaining to the project the README is for.
+  The goal was to create a command-l ine application that dynamically generates a README.md from a user's input. The application is invoked with the node index.js command. The user is prompted for their GitHub username and other information pertaining to the project the README is for.
 
   # Table of Contents
 
@@ -19,19 +17,18 @@
   * [Questions](#Questions)
   
   ## Installation
-  >1. Fork the Github repository. 
-  >2. Clone the forked repo into your local machine usin g gitbash/terminal to pull the project and data. 
-  >3. Access the code files and assets via Visual Studio or in your browser to view the code and website respectively. 
-  >4. Run npm init. 
-  >5. Install inquirer with command line npm install inquirer. 
-  >6. Install axios with command line npm install axios. 
-  >7. In the terminal , cd into the repo that contains the index.js file.
-  >8. Enter node index.j s in your terminal and answer the question prompts. 
-  >9. Open Github and use the Good README.md Generator for a repo.
-  >10. Answer the question prompts to generate the README.md. 
+  > 1. Fork t he Github repository. 
+  > 2. Clone the forked repo into your local machine usin g gitbash/terminal to pull the project and data. 
+  > 3. Access the code files and assets via Visual Studio or in your browser to view the code and website respectively. 
+  > 4. Run npm init. 
+  > 5. Install inquirer with command line npm install inquirer. 
+  > 6. In the terminal, cd into the repo that con tains the index.js file. 
+  > 7. Enter node index.js in your terminal and answer the question prompts. 
+  > 8. Open Github and use the Good README.md Generator for a repo.
+  > 9. Answer the question prompts to generate the README.md.
 
   ## Usage
-  > Key Components Used: node.js - ES6+ - NPM - inquirer - axios.
+  > Key Components Used: node.js - ES6+ - NPM - inquirer
 
   ## License
   MIT license. 
--- index.js
+++ index.js
@@ -7,67 +7,55 @@ const generateMarkdown = require('./utils/generateMarkdown');
 const util = require('util');
 const writeFileAsync = util.promisify(fs.writeFile);
 
-// create user questions array
-const userQuestions = [
-    "What is your GitHub username?",
-    "What is your Project Title?",
-    "Please include a short project description.",
-    "What commands are required to install the project and its dependencies?",
-    "Please provide repo usage instructions.",
-    "Please enter the License name if aplicable, otherwise enter N/A.",
-    "Please include Github usernames of project contributors, if applicable?",
-    "What commands are required to run tests?",
-];
-
 // function to run inquirer and promp user questions for project
 function questionPrompts() {
     return inquirer.prompt([
         {
             type: "input",
             name: "Username",
-            message: userQuestions[0]
+            message: "What is your GitHub username?"
         },
         {
             type: "input",
             name: "Title",
-            message: userQuestions[1]
+            message: "What is your Project Title?"
         },
         {
             type: "input",
             name: "Description",
-            message: userQuestions[2]
+            message: "Please include a short project description."
         },
         {
             type: "input",
             name: "Installation",
-            message: userQuestions[3]
+            message: "What commands are required to install the project and its dependencies?"
         },
         {
             type: "input",
             name: "Usage",
-            message: userQuestions[4]
+            message: "Please provide repo usage instructions."
         },
         {
             type: "checkbox",
             name: "License",
-            message: userQuestions[5],
+            message:     "Please enter the License name if aplicable, otherwise enter N/A.",
             choices: ["MIT", "Apache","GPL", "N/A", "Other"]
         },
         {
             type: "input",
             name: "Contribution",
-            message: userQuestions[6]
+            message:     "Please include Github usernames of project contributors, if applicable?"
         },
         {
             type: "input",
             name: "Tests",
-            message: userQuestions[7]
+            message: "What commands are required to run tests?"
         },
     
         {
             type: "input",
             name: "Email",
-            message: userQuestions[8]
+            message: "What is your emails?"
         }
         
     ])
