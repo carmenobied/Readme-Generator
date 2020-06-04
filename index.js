a8733ba040390be06fbcebb64b93b8421b0db649
Carmen Macleroy Obied, PhD <carmen.obied@gmail.com>
2020-06-04 00:26:47 -0700

    refactored code, questions working

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const inquirer = require("inquirer");
 const axios = require("axios");
 // import generateMarkdown file
-const generateREADME = require("./utils/generateMarkdown.js");
+const generateMarkdown = require('./utils/generateMarkdown');
 const util = require("util"); 
 // promisify to convert function into async function and return a promise
 const writeFileAsync = util.promisify(fs.writeFile);
@@ -50,39 +50,28 @@ function questionPrompts() {
         type: "input",
         message: "What commands are required to run tests?",
         name: "Tests"
-    }
-]);
+      }
+        
+    ])
+    // .then(answers => console.log(answers))
 }
 
-function generateREADME(answers) {
-return inquirer.prompt(answers);
-};
+// function to write README file
+function writeToFile(fileName, data) {
+    const markDown = generateMarkdown.generateMarkdown(data);
 
-
-async function init() {
-console.log("Hi! Let's start generating your README file.")
-try {
-  const answers = await questionPrompts();
-
-  module.exports = data
-
-  const goodREADME = generateREADME(answers);
-
-  await writeFileAsync("README.md", goodREADME);
-
-  console.log("Successfully wrote to README.md file");
-} catch(err) {
-  console.log(err);
-}
+    writeFileAsync(fileName, markDown);
 }
 
-// create badge via http://shields.io/
+// function to initialize program
+function init() {
+        questionPrompts()
+        .then( data => {
+            return writeToFile("README.md", data)
+        })
+        .then( () => console.log("Successfully created a README.md!") )
+        .catch( error => console.log(error) )
+}
 
-// call markdown generator
-
-// retrieve user's Github information via the Github API
-
-// call writeToFile() function
-
-// call init() function
+// function call to initialize program
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,54 @@
+// function to generate markdown for README
 function generateMarkdown(data) {
-  return `
-# ${data.title}
+    return `
+  # ${data.title}
 
-`;
-}
+  ![License Badge](https://img.shields.io/badge/license-${data.license[0]}-blue)
 
-module.exports = generateMarkdown;
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](#questions)
+    * 
+  ## Installation
+
+
+  ${data.install}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## License
+
+  This project is covered under the ${data.license[0]} license. For more information go to [https://choosealicense.com/licenses/](https://choosealicense.com/licenses/)
+
+  ## Contributing
+
+  ${data.contribution}
+
+  ## Tests
+  
+  ${data.tests}
+
+  ## Questions
+
+  [${data.username}](https://github.com/${data.username})
+
+  ${data.email}
+  
+  If you have any questions about the project you can reach me at the above email.
+  `;
+  }
+  
+  module.exports = 
+  {
+    generateMarkdown: generateMarkdown
+  }
